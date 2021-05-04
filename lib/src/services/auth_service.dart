64fc0319b0d585e8241df078bb4361338825863d
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-04 21:47:57 +0530

    Edit all methods to work with 2 factor auth API

--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -1,24 +1,37 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
+import 'dart:math';
+import 'package:http/http.dart' as http;
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-class AuthService {
-  static const key = "0dd286b6-8882-11eb-a9bc-0200cd936042";
+  static const _key = "0dd286b6-8882-11eb-a9bc-0200cd936042";
+  static const _chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  String _otp;
   Future<bool> requestOtp(String phoneNumber) async {
-    var response = await http.get(
-        Uri.parse('https://2factor.in/API/V1/$key/SMS/$phoneNumber/AUTOGEN'));
-    Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-    String status = decodedResponse['status'];
-    if (status == 'success')
+    _otp = _generateRandomString();
+    try {
+      var response = await http.get(
+          Uri.parse('https://2factor.in/API/V1/$_key/SMS/$phoneNumber/$_otp'));
+      Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+      String status = decodedResponse['status'];
+      if (status == 'success')
+        return true;
+      else
+        return false;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  bool confirmOtp(String otpEntered) {
+    if (_otp == otpEntered)
       return true;
     else
       return false;
   }
 
-  Future<void> signInwithOtp(String verId, String code) async {
-    var creds =
-        PhoneAuthProvider.credential(verificationId: verId, smsCode: code);
-    await _signIn(creds);
+  String _generateRandomString() {
+    return List.generate(8, (index) => _chars[Random().nextInt(_chars.length)])
+        .join();
   }
 }
